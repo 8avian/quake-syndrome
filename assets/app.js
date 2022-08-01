9a761efcfd0e29eed1e2347205793dff7847f93d
Octavian Bonea <octavian.bonea@emag.ro>
2022-08-01 15:54:24 +0300

    First usable version of the website

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import $ from 'jquery';
+import 'bootstrap';
+
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
